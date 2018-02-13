9ad0dde7729df630a0010cca6c680fdc305ed20e
Raaghu <krraghavendra@yahoo.co.in>
2018-02-13 12:22:05 +0530

    fixed misspelled name in Persistence Exception

--- src/Errors/Exceptions/Persistence/PersistenceException.php
+++ src/Errors/Exceptions/Persistence/PersistenceException.php
@@ -6,6 +6,6 @@ use PhpPlatform\Errors\Exceptions\PlatformException;
 
 abstract class PersistenceException extends PlatformException {
 	public function __construct($message = null, $code = null, $previous = null) {
-		parent::__construct ("Persistance", "[P]", $message, $code , $previous );
+		parent::__construct ("Persistence", "[P]", $message, $code , $previous );
 	}
 }
